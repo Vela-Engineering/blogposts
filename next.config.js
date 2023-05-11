76a300b2895ded9a050edc8114c65461f1f96f60
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-05-11 02:49:59 +0000

    Replace favicons with new favicons

--- next.config.js
+++ next.config.js
@@ -19,4 +19,28 @@ module.exports = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/outstatic",
+        destination: "/studio",
+        permanent: true,
+      },
+      {
+        source: "/outstatic/:slug",
+        destination: "/studio/:slug", // Matched parameters can be used in the destination
+        permanent: true,
+      },
+      {
+        source: "/api/outstatic",
+        destination: "/api/studio",
+        permanent: true,
+      },
+      {
+        source: "/api/outstatic/:slug",
+        destination: "/api/studio/:slug", // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ];
+  },
 };
