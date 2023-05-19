c2a0c6aa2ef78b947b5bdfe7b7cd27c626d664e4
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2023-05-19 21:42:09 +0000

    Fix bug to create new blog post

--- next.config.js
+++ next.config.js
@@ -19,28 +19,4 @@ module.exports = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/outstatic",
-        destination: "/studio",
-        permanent: true,
-      },
-      {
-        source: "/outstatic/:slug",
-        destination: "/studio/:slug", // Matched parameters can be used in the destination
-        permanent: true,
-      },
-      {
-        source: "/api/outstatic",
-        destination: "/api/studio",
-        permanent: true,
-      },
-      {
-        source: "/api/outstatic/:slug",
-        destination: "/api/studio/:slug", // Matched parameters can be used in the destination
-        permanent: true,
-      },
-    ];
-  },
 };
